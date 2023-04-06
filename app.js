1d2ec62ea3848b076b82450044870bd92add7a04
Alexc <alexsejsi4@gmail.com>
2023-04-06 12:16:48 +0300

    Add app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+
+app.use((err, req, res, next) => {
+  console.log('err :>> ', err);
+  res.status(500).send('Server Error');
+});
+
+module.exports = app;
